85a2709b6ba617cfbe86a9637690cd9b66aaddeb
Ty Cobb <derkis.cobb@gmail.com>
2011-08-12 15:15:29 -0500

    user tests -- fixed

--- db/schema.rb
+++ db/schema.rb
@@ -15,9 +15,10 @@ ActiveRecord::Schema.define(:version => 20110811034319) do
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
+    t.string   "encrypted_password"
+    t.string   "salt"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "encrypted_password"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
