ebd5a7b3721c2787686a56cc1b05efde8f846e0f
Ty Cobb <derkis.cobb@gmail.com>
2011-08-14 16:41:17 -0500

    fixed user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,14 @@ class UsersController < ApplicationController
   def new
     @title = 'Register'
   end
+  
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      redirect_to "" # TODO redirect to lobby
+    else
+      @title = "Register"
+      render 'new'
+    end
+  end
 end
