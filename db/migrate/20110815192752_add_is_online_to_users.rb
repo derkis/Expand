248ec049cd6a6b3dc721fc58e0de6611bc43241b
Ty Cobb <derkis.cobb@gmail.com>
2012-03-20 13:29:49 -0500

    fresh start

--- db/migrate/20110815192752_add_is_online_to_users.rb
+++
@@ -1,9 +0,0 @@
-class AddIsOnlineToUsers < ActiveRecord::Migration
-  def self.up
-    add_column :users, :is_online, :boolean
-  end
-
-  def self.down
-    remove_column :users, :is_online
-  end
-end
--- public/javascripts/application.js
+++
@@ -1,7 +0,0 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
-function show_popup() {
-	$(".login_popup").show();
-	$(".transparency").show();
-	return false;
-}
