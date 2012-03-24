b4b4658e20005256bcffbbfc1bedbe74f41e7fae
Ty Cobb <derkis.cobb@gmail.com>
2012-03-24 14:18:41 -0500

    add players and model relationships

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,5 @@
 class Game < ActiveRecord::Base
+  has_many :players
+  has_many :users, :through => :players
   
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+  has_one :player
+  has_one :game, :through => :player
+  
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, 
