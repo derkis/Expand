96505e25af72631496bdb61afd97562a4bcce467
Ty Cobb <derkis.cobb@gmail.com>
2012-07-15 22:55:26 -0500

    added homepage styling, which is broken in firefox but less broken in other browsers

--- app/assets/javascripts/home/index.js
+++ app/assets/javascripts/home/index.js
@@ -1,3 +1,38 @@
+var signin_popover_visible = false;
+
 $(document).ready(function() {
-	console.log('loaded the home#index script');
+
+	// signin popover setup
+	var signin_popover_position = $('.signin_button').offset();
+	console.log('signin_popover_position: ' + signin_popover_position.left + ' ' + signin_popover_position.top);
+	$('.title').load(function() { // wait for image to load
+		signin_popover_position.left += $('.title').outerWidth() + $('.signin_button').outerWidth();
+		signin_popover_position.top -= $('.signin_popover').outerHeight() / 2 - $('.signin_button').outerHeight() / 2;
+		signin_popover_position.top -= $('#mainbar').offset().top;
+		$('.signin_popover').css({ 'left': signin_popover_position.left, 'top': signin_popover_position.top });
+	});
+
+	// handlers for hiding popovers
+	$('body').click(function() {
+		if(signin_popover_visible) {
+			$('.signin_button').attr('src', 'assets/signin_button.png');
+			$('.signin_popover').fadeOut(50);
+			signin_popover_visible = false;
+		}
+	})
+
+	$('.signin_popover').click(function(event) {
+		return false;
+	});
+
+	// handlers for showing popovers 
+	$('.signin_button').mouseenter(function() {
+		console.log(signin_popover_position);
+		if(!signin_popover_visible) {
+			$('.signin_button').attr('src', 'assets/signin_button_hover.png');
+			$('.signin_popover').fadeIn('fast');
+			signin_popover_visible = true;
+		}
+	});
+
 });
