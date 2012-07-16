fad8fcfe8a6eb344216ec70c20874e506450bea7
Ty Cobb <tcobb@roundarch.com>
2012-07-16 18:01:55 -0500

    changed color scheme

--- app/assets/javascripts/home/index.js
+++ app/assets/javascripts/home/index.js
@@ -6,8 +6,9 @@ $(document).ready(function() {
 	var signin_popover_position = $('.signin_button').offset();
 	console.log('signin_popover_position: ' + signin_popover_position.left + ' ' + signin_popover_position.top);
 	$('.title').load(function() { // wait for image to load
+		console.log('singin_popover height: ' + $('.signin_popover').outerHeight());
 		signin_popover_position.left += $('.title').outerWidth() + $('.signin_button').outerWidth();
-		signin_popover_position.top -= $('.signin_popover').outerHeight() / 2 - $('.signin_button').outerHeight() / 2;
+		signin_popover_position.top -= get_popover_height('.signin_popover') / 2 - $('.signin_button').outerHeight() / 2;
 		signin_popover_position.top -= $('#mainbar').offset().top;
 		$('.signin_popover').css({ 'left': signin_popover_position.left, 'top': signin_popover_position.top });
 	});
@@ -31,8 +32,15 @@ $(document).ready(function() {
 		if(!signin_popover_visible) {
 			$('.signin_button').attr('src', 'assets/signin_button_hover.png');
 			$('.signin_popover').fadeIn('fast');
+			$('.signin_popover').css({ 'display': 'inline-block' });
 			signin_popover_visible = true;
 		}
 	});
 
 });
+
+function get_popover_height(popover) {
+	var border_size = $(popover).css('border-image-slice').split(" "); // [top, right, bottom, left]
+	var shadow_size = border_size[2] - border_size[0];
+	return $(popover).outerHeight() - shadow_size;
+}
