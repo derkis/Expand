06f09f67931cd820727001d10e34d99b7cdd6e70
Ty Cobb <derkis.cobb@gmail.com>
2012-07-17 21:43:43 -0500

    styled homepage in a way that looks good in my browser and resolution and bad anywhere else

--- app/assets/javascripts/home/index.js
+++ app/assets/javascripts/home/index.js
@@ -1,46 +1,60 @@
-var signin_popover_visible = false;
+var visible_popover = null;
+var signin_button = '#signin_button';
+var signin_popover = '#signin_form';
+var signup_button = '#signup_button';
+var signup_popover = '#signup_form';
 
 $(document).ready(function() {
 
-	// signin popover setup
-	var signin_popover_position = $('.signin_button').offset();
-	console.log('signin_popover_position: ' + signin_popover_position.left + ' ' + signin_popover_position.top);
+	// popover setup
 	$('.title').load(function() { // wait for image to load
-		console.log('singin_popover height: ' + $('.signin_popover').outerHeight());
-		signin_popover_position.left += $('.title').outerWidth() + $('.signin_button').outerWidth();
-		signin_popover_position.top -= get_popover_height('.signin_popover') / 2 - $('.signin_button').outerHeight() / 2;
-		signin_popover_position.top -= $('#mainbar').offset().top;
-		$('.signin_popover').css({ 'left': signin_popover_position.left, 'top': signin_popover_position.top });
-	});
-
-	// handlers for hiding popovers
-	$('body').click(function() {
-		if(signin_popover_visible) {
-			$('.signin_button').attr('src', 'assets/signin_button.png');
-			$('.signin_popover').fadeOut(50);
-			signin_popover_visible = false;
-		}
-	})
-
-	$('.signin_popover').click(function(event) {
-		return false;
+		set_popover_position(signin_button, signin_popover);
+		set_popover_position(signup_button, signup_popover);
+		fade_in_popover_if_necessary(signin_popover);
 	});
 
 	// handlers for showing popovers 
-	$('.signin_button').mouseenter(function() {
-		console.log(signin_popover_position);
-		if(!signin_popover_visible) {
-			$('.signin_button').attr('src', 'assets/signin_button_hover.png');
-			$('.signin_popover').fadeIn('fast');
-			$('.signin_popover').css({ 'display': 'inline-block' });
-			signin_popover_visible = true;
-		}
+	$(signin_button).mouseenter(function() {
+		fade_in_popover_if_necessary(signin_popover);
+	});
+	$(signup_button).mouseenter(function() {
+		fade_in_popover_if_necessary(signup_popover);
 	});
 
+	/* handlers for hiding popovers
+	$('body').click(function() {
+		if(visible_popover) {
+			$(visible_popover).fadeOut('fast');
+			visible_popover = null;
+		}
+	})
+
+	$('.popover').click(function(event) {
+		return false;
+	});
+	*/
+	
 });
+
+function set_popover_position(button, popover) {
+	var popover_position = $(button).offset();
+	popover_position.left += $(button).outerWidth(); // $('.title').outerWidth() + 
+	popover_position.top -= get_popover_height(popover) / 2 - $(button).outerHeight() / 2;
+	popover_position.top -= $('#mainbar').offset().top + $('.buttons').position().top;
+	$(popover).css({ 'left': popover_position.left, 'top': popover_position.top });
+}
 
 function get_popover_height(popover) {
 	var border_size = $(popover).css('border-image-slice').split(" "); // [top, right, bottom, left]
 	var shadow_size = border_size[2] - border_size[0];
 	return $(popover).outerHeight() - shadow_size;
+}
+
+function fade_in_popover_if_necessary(popover) {
+	if(visible_popover != popover) {
+		$(visible_popover).fadeOut('fast');
+		$(popover).fadeIn('fast');
+		$(popover).css({ 'display': 'inline-block' });
+		visible_popover = popover;
+	}
 }
