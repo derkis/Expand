7bce1ab75771780ba6128e0d81c42bf7e949d81d
Ty Cobb <derkis.cobb@gmail.com>
2012-07-30 21:55:39 -0500

    added basic place_piece PUT action to the client

--- app/assets/javascripts/games/show.js
+++ app/assets/javascripts/games/show.js
@@ -1,4 +1,9 @@
-chars = ["A","B","C","D","E","F","G","H","I","J","K","L"];
+var chars = ["A","B","C","D","E","F","G","H","I","J","K","L"];
+
+var GAME_ID;
+var TURN_TYPE = {
+    PLACE_PIECE: { code: 0, name: 'place_piece' }
+};
 
 $(document).ready(function() {
     polling_wrapper();
@@ -7,51 +12,85 @@ $(document).ready(function() {
         function() {$(this).css("background-color", "gray")}, 
         function() {$(this).css("background-color", "white")}
     );
+
+    $('.game_cell').live('click', function(click_event) {
+        var cell = $(this);
+        send_place_piece_action(
+            parseInt(cell.attr('row')), 
+            parseInt(cell.attr('column'))
+        );
+    });
 });
 
-function polling_wrapper() {
-    $.getJSON(document.URL + '.json', null, function(data) {
-        console.log(data)
-        renderBoard(data.cur_turn.board, data.template.width, data.template.height);
-        renderPlayers(data.cur_turn)
-    });
+$('#game').ready(function() {
+    GAME_ID = $('#game').attr('game_id');
+});
 
+// polling functions
+function polling_wrapper() {
+    fetch_game_state();
     setTimeout(polling_wrapper, 15000);
 }
 
-function renderPlayers(cur_turn)
-{
-    playerDiv = "#player_" + cur_turn.player_id;
-    $(playerDiv).css("background-color", "red");
+function fetch_game_state() {
+    $.getJSON(document.URL + '.json', function(game_state) {
+        console.log(game_state);
+        render_board(game_state.cur_turn.board, game_state.template.width, game_state.template.height);
+        render_players(game_state.cur_turn);
+    });
 }
 
-function renderBoard(boardStr, width, height) {
-    r = 0;
-    c = 0;
+// server calls
 
-    for (chr = 0; chr < boardStr.length; chr++)
-    {
-        renderCell(r, c, boardStr[chr], $("#cell" + r + "_" + c));
+function send_game_update(action) {
+    var json_update = { 'game': { 'id': GAME_ID, 'turn': { 'action': action } } };
 
-        c++;
-
-        if (c >= width)
-        {
-            c = 0;
-            r++;
+    $.ajax({
+        type: 'PUT',
+        url: GAME_ID + '.json',
+        data: JSON.stringify(json_update), 
+        contentType: 'application/json',                  
+        dataType: 'json',                                  
+        success: function(response) {                          
+            console.log('game update');
+            console.log(repsonse);
         }
+    });     
+}
+
+function send_place_piece_action(row, column) {
+    send_game_update({
+        'turn_type': TURN_TYPE.PLACE_PIECE.code,
+        'row'      : row,
+        'column'   : column
+    });
+}
+
+// board rendering helpers
+function render_board(board, num_columns, num_rows) {
+    var row = 0, column = 0;
+    for(var cell_index=0 ; cell_index < board.length ; cell_index++) {    
+        var cell_id = '#cell_' + row + '_' + column;
+        render_cell($(cell_id), board[cell_index], row, column);
+        
+        column = (column + 1) % num_columns;
+        if(column == 0) 
+            row += 1;
     }
 }
 
-function renderCell(row, column, type, cell)
-{
-    switch (type)
-    {
-        case "e":
-            cell.css("background-color", "gray");
-            cell.text(chars[r] + (c+1));
-        break;
+function render_cell(cell, cell_type, row, column) {
+    switch(cell_type) {
+        case 'e':
+            cell.css('background-color', 'gray');
+            cell.text(chars[row] + (column+1));
+            break;
         default:
-            cell.text(type);
+            cell.text(cell_type);
     }
+}
+
+function render_players(current_turn) {
+    playerDiv = "#player_" + current_turn.player_id;
+    $(playerDiv).css("background-color", "red");
 }
