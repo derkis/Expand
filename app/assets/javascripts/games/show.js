0f3316ba0684ebcb2912ff208574f5da9773e96e
jjung <jjung@roundarch.com>
2012-10-10 00:03:34 -0500

    Got the javascript formatted and properly highlighting adjacent cells.

--- app/assets/javascripts/games/show.js
+++ app/assets/javascripts/games/show.js
@@ -1,17 +1,62 @@
-player_index = -1;
+//------------------------------------------------------------------------------------------
+//
+// Lib
+//
+//------------------------------------------------------------------------------------------
+function hasClass(element, cls)
+{
+    var r = new RegExp('\\b' + cls + '\\b');
+    return r.test(element.className);
+}
 
-Number.prototype.to_char = function() {
+function hasClasses(element, classes)
+{
+    for (var i = 0; i < classes.length; i++)
+    {
+        var clazz = classes[i];
+        var r = new RegExp('\\b' + clazz + '\\b');
+        if (!r.test(element.className))
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+//------------------------------------------------------------------------------------------
+//
+// Variables
+//
+//------------------------------------------------------------------------------------------
+var player_index = -1;
+var current_turn_type;
+var selected_cell;
+var cur_game_state;
+
+//------------------------------------------------------------------------------------------
+//
+// Initialization
+//
+//------------------------------------------------------------------------------------------
+$(document).ready(document_readyHandler);
+$('#game').ready(game_readyHandler);
+
+Number.prototype.to_char = function()
+{
     return String.fromCharCode(this);
 }
 
-String.prototype.format = function() {
+String.prototype.format = function()
+{
     var args = arguments;
     return this.replace(/{(\d+)}/g, function(match, number) { 
         return typeof args[number] != 'undefined' ? args[number] : match;
     });
 };
 
-String.prototype.to_int = function() {
+String.prototype.to_int = function() 
+{
     return parseInt(this);
 }
 
@@ -65,58 +110,27 @@ var TURN_TYPES = {
     }
 };
 
-var current_turn_type;
-var selected_cell;
-
-$(document).ready(function() {
-    polling_wrapper();
-
-    $('.enabled').live('click', function(click_event) {
-        if(!player_can_act())
-            return;
-        var cell = $(this);
-        if(selected_cell && selected_cell != cell)
-            selected_cell.removeClass('selected');
-        selected_cell = cell;
-        selected_cell.addClass('selected');
-    });
-});
-
-$('#game').ready(function() {
-    GAME_ID = $('#game').attr('game_id');
-});
-
+//------------------------------------------------------------------------------------------
+//
+// Methods
+//
+//------------------------------------------------------------------------------------------
 // polling functions
-function polling_wrapper() {
+function polling_wrapper()
+{
     fetch_game_state();
+
     setTimeout(polling_wrapper, 15000);
 }
 
-function fetch_game_state() {
-    $.getJSON(document.URL + '.json', function(game_state) {
-        console.log(game_state);
-
-        current_turn_type = (function(turn_code) { 
-            for(var type_key in TURN_TYPES)
-                if(TURN_TYPES[type_key].code == turn_code)
-                    return TURN_TYPES[type_key];
-        })(game_state.valid_action.code);
-
-        var turn_button = $('#turn_button');
-        if(player_can_act()) 
-            turn_button.removeAttr('disabled');
-        else turn_button.attr('disabled', 'disabled');
-
-        player_index = game_state.current_player_index.toString()
-        current_turn_type.render();
-        render_board(game_state.current_turn.board, game_state.template.width, game_state.template.height);
-        render_players(game_state.current_turn);
-        render_metadata(game_state);
-    });
+function fetch_game_state()
+{
+    $.getJSON(document.URL + '.json', fetch_game_state_resultHandler);
 }
 
 // server calls
-function send_game_update(action) {
+function send_game_update(action)
+{
     var json_update = { 'game': { 'turn': { 'action': action } } };
 
     $.ajax({
@@ -125,14 +139,12 @@ function send_game_update(action) {
         data: JSON.stringify(json_update), 
         contentType: 'application/json',                  
         dataType: 'json',                                  
-        success: function(response)
-        {
-            fetch_game_state();
-        }
+        success: send_game_update_successHandler
     });     
 }
 
-function create_action_and_send_game_update() {
+function create_action_and_send_game_update()
+{
     var action = { 'turn_type' : current_turn_type.name };
     var action_data = current_turn_type.build_action();
 
@@ -141,28 +153,32 @@ function create_action_and_send_game_update() {
     send_game_update(action)
 }
 
-function build_start_company_action() {
+function build_start_company_action()
+{
     // create_action_and_send_game_update(
     //     TURN_TYPES.CHOOSE_COMPANY,
     //     { 'company': company }
     // );
 }
 
-function build_purchase_stock_action() {
+function build_purchase_stock_action()
+{
     // create_action_and_send_game_update(
     //     TURN_TYPES.PURCHASE_STOCK,
     //     { 'purchases': purchases }
     // );
 }
 
-function build_trade_stock_action() {
+function build_trade_stock_action()
+{
     // create_action_and_send_game_update(
     //     TURN_TYPES.TRADE_STOCK,
     //     { 'trades': trades }
     // );
 }
 
-function build_merge_order_action() {
+function build_merge_order_action()
+{
     // create_action_and_send_game_update(
     //     TURN_TYPES.MERGE_ORDER,
     //     { 'merge_order': merge_order }
@@ -171,11 +187,13 @@ function build_merge_order_action() {
 
 // board rendering helpers
 
-function render_no_action_turn() {
+function render_no_action_turn()
+{
     console.log('render no action');
 }
 
-function render_place_piece_turn() {
+function render_place_piece_turn()
+{
     console.log('render place piece');
     // send_place_piece_action(
     //     parseInt(cell.attr('row')), 
@@ -183,31 +201,38 @@ function render_place_piece_turn() {
     // );
 }
 
-function render_start_company_turn() {
+function render_start_company_turn()
+{
 
 }
 
-function render_purchase_stock_turn() {
+function render_purchase_stock_turn()
+{
 
 }
 
-function render_trade_stock_turn() {
+function render_trade_stock_turn()
+{
 
 }
 
-function render_merge_order_turn() {
+function render_merge_order_turn()
+{
 
 }
 
-function reset_game() {
+function reset_game()
+{
     send_game_update({ 'turn_type' : 'reset' });
 }
 
-function render_metadata(game_state) {
+function render_metadata(game_state)
+{
     $('.turn_label').text("Turn: " + game_state.current_turn.number);
 }
 
-function render_board(board, num_columns, num_rows) {
+function render_board(board, num_columns, num_rows)
+{
     $('.debug_string').text(board);
 
     var row = 0, column = 0;
@@ -221,7 +246,8 @@ function render_board(board, num_columns, num_rows) {
     }
 }
 
-function render_cell(cell, cell_type, row, column) {
+function render_cell(cell, cell_type, row, column)
+{
     cell.removeClass('empty');
     cell.removeClass('enabled');
     cell.removeClass('no_hotel');
@@ -240,11 +266,177 @@ function render_cell(cell, cell_type, row, column) {
     }
 }
 
-function render_players(current_turn) {
+function render_players(current_turn)
+{
     $('.player').removeClass('current_player');
     $('.player[player_id=' + current_turn.player_id + ']').addClass('current_player');
 }
 
-function player_can_act() {
+function player_can_act()
+{
     return current_turn_type != TURN_TYPES.NO_ACTION;
+}
+
+function get_cell_at(row, column)
+{
+    return cur_game_state.current_turn.board.chartAt(row * cur_game_state.template.width + column)
+}
+
+function get_cell_at(row, column)
+{
+    return $('#cell_' + row + "_" + column);
+}
+
+function get_key(cell)
+{
+    var row = get_row(cell);
+    var col = get_column(cell);
+    return row + "_" + col;
+}
+function get_row(cell)
+{
+    return parseInt(cell.attributes.row.value)
+}
+
+function get_column(cell)
+{
+    return parseInt(cell.attributes.column.value)
+}
+
+function get_adjacent_cells(cell, classes)
+{
+    var map = {};
+    get_adjacent_cells_recurse(cell, classes, map);
+    return map;
+}
+
+function get_adjacent_cells_recurse(cell, classes, map)
+{
+    var row = get_row(cell);
+    var column = get_column(cell);
+
+    var left = column > 0 ? get_cell_at(row, column - 1) : null;
+    var right = column < cur_game_state.template.width - 1 ? get_cell_at(row, column + 1) : null;
+    var top = row > 0 ? get_cell_at(row - 1, column) : null;
+    var bottom = row < cur_game_state.template.height - 1 ? get_cell_at(row + 1, column) : null;
+    var all = [left, right, top, bottom];
+
+    for (var i = 0; i < 4; i++)
+    {
+        var cur_cell = all[i];
+
+        if (cur_cell && !map[get_key(cur_cell[0])] && hasClasses(cur_cell[0], classes))
+        {
+            map[get_key(cur_cell[0])] = cur_cell;
+            get_adjacent_cells_recurse(cur_cell[0], classes, map);
+        }
+    }
+}
+
+//------------------------------------------------------------------------------------------
+//
+// Events
+//
+//------------------------------------------------------------------------------------------
+function fetch_game_state_resultHandler(game_state)
+{
+    console.log(game_state);
+
+    cur_game_state = game_state;
+
+    current_turn_type = (function(turn_code) { 
+        for(var type_key in TURN_TYPES)
+            if(TURN_TYPES[type_key].code == turn_code)
+                return TURN_TYPES[type_key];
+    })(game_state.valid_action.code);
+
+    var turn_button = $('#turn_button');
+
+    if(player_can_act()) 
+    {
+        turn_button.removeAttr('disabled');
+    }
+    else
+    { 
+        turn_button.attr('disabled', 'disabled');
+    }
+
+    player_index = game_state.current_player_index.toString()
+    current_turn_type.render();
+    render_board(game_state.current_turn.board, game_state.template.width, game_state.template.height);
+    render_players(game_state.current_turn);
+    render_metadata(game_state);
+}
+
+function document_readyHandler()
+{
+    polling_wrapper();
+
+    $('.enabled').live('click', enabled_clickHandler);
+    $('.cell').hover(cell_hoverOverHandler, cell_hoverOutHandler);
+}
+
+function game_readyHandler()
+{
+    GAME_ID = $('#game').attr('game_id');
+}
+
+function enabled_clickHandler(click_event)
+{
+    if(!player_can_act())
+    {
+        return;
+    }
+
+    var cell = $(this);
+
+    if(selected_cell && selected_cell != cell)
+    {
+        selected_cell.removeClass('selected');
+    }
+    selected_cell = cell;
+    selected_cell.addClass('selected');
+}
+
+function cell_hoverOverHandler(click_event)
+{
+    var cell = click_event.currentTarget;
+
+    if(!player_can_act() || !hasClass(cell, "enabled"))
+    {
+        return;
+    }
+
+    adjacent_cells = get_adjacent_cells(cell, ["no_hotel"]);
+
+    for (key in adjacent_cells)
+    {
+        var cur_cell = adjacent_cells[key];
+
+        cur_cell.addClass('adjacent');
+    }
+}
+
+function cell_hoverOutHandler(click_event)
+{
+    var cell = click_event.currentTarget;
+
+    if(!player_can_act() || !hasClass(cell, "enabled"))
+    {
+        return;
+    }
+
+    adjacent_cells = get_adjacent_cells(cell, ["no_hotel"]);
+
+    for (key in adjacent_cells)
+    {
+        var cur_cell = adjacent_cells[key];
+
+        cur_cell.removeClass('adjacent');
+    }
+}
+
+function send_game_update_successHandler()
+{
+    fetch_game_state();
 }
