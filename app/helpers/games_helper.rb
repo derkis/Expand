243e943dda9e8375e7f1e92ff48636d8662df859
Ty Cobb <derkis.cobb@gmail.com>
2012-10-09 20:45:40 -0500

    removed some dead code from games helper

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,7 +1,5 @@
 module GamesHelper
   
-  CHARS = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J","K"]
-
   def verify_user_in_game
     redirect_to :portal unless current_user.can_view_game?(params[:id])
   end
@@ -12,39 +10,13 @@ module GamesHelper
   end
     
   def get_board_array_from_game(game)
-    board = Array.new(game.template.height) { Array.new }
-
-    r = 0
-    ix = 0
-
-    while r < game.template.height
-      c = 0
-
-      while c < game.template.width
-        lastTurn = game.current_turn
-        board[r][c] = lastTurn.board[ix]
-        ix = ix + 1
-        c = c + 1
+    linear_index = 0
+    Array.new(game.template.height){ Array.new }.each do |row_array|
+      game.template.width.times do |column|
+        row_array[column] = game.current_turn.board[linear_index]
+        linear_index += 1
       end
-      r = r + 1
     end
-
-    return board
   end
   
-  def find_tiles_for (playerIX, board)
-    ret = Array.new
-    board.chars.to_a.each_with_index do |c, i|
-      ret.push(i) if c.ord - 48 == playerIX
-    end
-    ret.sort
-    ret
-  end
-
-  def tile_ix_to_name (ix, template)
-    row = ix / template.width
-    col = ix % template.width + 1
-
-    CHARS[row] + col.to_s
-  end
 end
